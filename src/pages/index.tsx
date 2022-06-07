bb680969ba72e40e487e49b85ab850fc27efbf26
Yan Lyra <yanlyra3@gmail.com>
2022-06-06 21:23:11 -0300

    feat: initial deploy

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>What my Age in Other Planet?</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
